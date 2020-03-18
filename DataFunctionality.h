96dbd8879c05446df67746b56754fb8d94bd59e2
bogdan <bogdanmica36@gmail.com>
2020-03-18 08:10:28 +0200

    functii de search si add pt string si int

--- DataFunctionality.h
+++ DataFunctionality.h
@@ -1,3 +1,8 @@
+#include "structures.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /* Data definition    ->Init DB 
 				      ->Delete DB -> Free memory
 				      ->Create Table
@@ -11,17 +16,22 @@
    					  ->Clear table: free all lines
  */
 
-#include "structures.h"
-#include <string.h>
-#include<stdlib.h>
-#include<stdio.h>
-
 /* data definition:  ->t_db *initDB(char *name)
 					 |
 					 -->aloc memorie pt tabele,coloane, linii 
 */
 
 //int query
+
+t_intCell *alocIntCell(int value)
+{
+	t_intCell *aux = malloc(sizeof(t_intCell));
+	if(!aux)
+		return NULL;
+	aux->value = value;
+	return aux;
+}
+
 void printIntCells(t_intCell *cells)
 {
 	for(;cells != NULL; cells = cells->next)
@@ -36,30 +46,41 @@ void printIntLines(t_intLine *lines)
 }
 
 
-void insertIntVal(t_table *table,int val[])
+void insertIntVal(t_table *table,int val[],int coloane)
 {
 	t_intLine *line ;
-	
 	/* tre sa stiu cate coloane sunt dinainte */
 	// iau cazul cu 2
-	t_intCell *aux = malloc(sizeof(t_intCell));
-	if(!aux)
-	{
-		printf("nu s a alocat\n");
-		return;
-	}
-	aux->value = val[0];
+	
 	line = malloc(sizeof(t_intLine));
 	if (!line)
 	{	printf("didn t alocate line \n");
-		free(aux);
 		return;
 	}
 
-	line->cells = aux;
-	aux = malloc(sizeof(t_intCell));
-	aux->value = val[1];
-	line->cells->next = aux;
+	t_intCell *ultim = NULL;
+	int i;
+	for( i = 0 ;i < coloane; i++)
+	{
+		t_intCell *aux = malloc(sizeof(t_intCell));
+		if(!aux)
+		{
+			printf("nu s a alocat\n");
+			return;
+		}
+		aux->value = val[i];
+		if(!line->cells)
+		{
+			line->cells = aux;
+			ultim = aux;
+		}
+		else if(ultim)
+		{
+			ultim->next = aux;
+			ultim = aux;
+		}
+	}
+	ultim->next = NULL;
 	// verific daca exista alta linie inainte in tabel
 	if(!table->lines)
 	table->lines = line;
@@ -73,17 +94,33 @@ void insertIntVal(t_table *table,int val[])
 	}
 
 }
-void addInt(t_db *db,char *tableName,int val[])
+void addInt(t_db *db,char *tableName,int val[],int coloane)
 {
 	t_table *tables = db->tables;
 
 	for(; tables != NULL; tables = tables->next)
 		if (!strcmp(tables->name,tableName))
 			break;
-	insertIntVal(tables,val);
+	insertIntVal(tables,val,coloane);
 }
 
+//void findINT()
 //string query
+
+t_stringCell *alocStringCell(char *val)
+{
+	t_stringCell *aux = malloc(sizeof(t_stringCell));
+	if(!aux)
+		return NULL;
+	aux->value = malloc(30);
+	if(!aux->value)
+	{
+		free(aux);
+		return NULL;
+	}
+	strcpy(aux->value,val);
+	return aux;
+}
 void printStringCells(t_stringCell *cells)
 {
 	for( ;cells != NULL ; cells = cells->next)
@@ -98,21 +135,118 @@ void printStringLines(t_stringLine *lines)
 }
 
 
-void insertStringVal(t_table *table,char **values)
+void insertStringVal(t_table *table,char **val,int coloane)
 {
+	// la fel ca la int iau prima oara cazul cu 2 coloane
 
+	//parcurg pana la ultima linie a tabelului in cazul in care exista
+	t_stringLine *line ;
+	line = malloc(sizeof(t_stringLine));
+	if(!line)
+	{
+		printf("nu s a alocat linie pt string\n");
+		return;
+	}
+	t_stringCell *ultim = NULL;
+	for(int i = 0; i < coloane ;i++){
+		if(!line->cells){
+			line->cells = alocStringCell(val[i]);
+			ultim = line->cells;
+		}
+		else if(ultim)
+		{
+			ultim->next = alocStringCell(val[i]);
+			ultim = ultim->next;
+		}
+	}
+	ultim->next = NULL;
+
+	
+	if(!table->lines)
+		{table->lines = line;
+		 
+		}
+	else
+	{
+		t_stringLine *p = table->lines;
+		for(; p->next != NULL; p = p->next);
+		p->next = line;
+		line->next = NULL;
+	}
+	
 }
-void addString(t_db *db,char *tableName,char **values)
+void addString(t_db *db,char *tableName,char **values,int coloane)
 {
 	t_table *tables = db->tables;
 
 	for(; tables != NULL; tables = tables->next)
 		if (!strcmp(tables->name,tableName))
 			break;
-	insertStringVal(tables,values);
+	if(tables)
+	insertStringVal(tables,values,coloane);
 }
 
+//am verificat doar pentru valori egale la cautare 
 
+int checkStringLine(t_stringLine *line,int whichColumn,char *value,char *rel)
+{
+	t_stringCell *cel = line->cells;
+	int coloana = 0;
+	for(; cel != NULL ; cel = cel->next,coloana++)
+	 {if(!strcmp(rel,"<"))
+		if (strcmp(value,cel->value) == 1)
+		 if(coloana == whichColumn )
+			return 1;
+	  if(!strcmp(rel,"<="))
+		if (strcmp(value,cel->value) == 1 || strcmp(value,cel->value) == 0)
+		 if(coloana == whichColumn )
+			return 1;
+	  if(!strcmp(rel,">"))
+		if (strcmp(value,cel->value) == -1)
+		 if(coloana == whichColumn )
+			return 1;
+	  if(!strcmp(rel,">="))
+		if (strcmp(value,cel->value) == -1 || strcmp(value,cel->value) == 0)
+		 if(coloana == whichColumn )
+			return 1;
+	  if(!strcmp(rel,"!="))
+		if (strcmp(value,cel->value) != 0)
+		 if(coloana == whichColumn )
+			return 1;
+	   if(!strcmp(rel,"=="))
+		if (strcmp(value,cel->value) == 0)
+		 if(coloana == whichColumn )
+			return 1;
+
+	 }
+
+	return 0;
+}
+void findSTRING(t_db *db,char *tableName,char *columnName,char *value,char *rel)
+{
+	t_table *t = db->tables;
+
+	for( ; t != NULL; t = t->next)
+		if (!strcmp(tableName,t->name))
+			break;	
+	int nr = 0;
+
+	if(t){
+	t_column *col = t->columns;
+	for(; col != NULL ; col = col->next,nr++)
+		if (!strcmp(col->name,columnName))
+			break;
+	if(col){
+			t_stringLine *lin = t->lines;
+			for(; lin != NULL ;lin = lin->next)
+				if (checkStringLine(lin,nr,value,rel))
+				 {
+				 	printStringLines(lin);
+				 	break;
+				 }
+			}
+		 }
+}
 
 t_db *initDB(char *name)
 {
@@ -225,6 +359,12 @@ t_table *createTable(t_db *db,char *tableName/*tipul pe care l las int prima oar
 {	
 	if(!db->tables)
 	{db->tables = initTable();
+	 if(!db->tables)
+	 {
+	 	printf("didn t alocate table\n");
+	 	free(db);
+	 	return NULL;
+	 }
 	strncpy(db->tables->name,tableName,strlen(tableName));
 	}
 	else
@@ -233,6 +373,11 @@ t_table *createTable(t_db *db,char *tableName/*tipul pe care l las int prima oar
 		for(; tables->next != NULL; tables = tables->next);
 
 		t_table *aux = initTable();
+		if(!aux)
+		{	deleteTable(db->tables);
+			free(db);
+			return NULL;
+		}
 		tables->next = aux;
 		aux->next = NULL;
 	}
--- main.c
+++ main.c
@@ -3,40 +3,174 @@
 #include "DataFunctionality.h"
 #include <string.h>
 
-
 #define DELIM " "
 
 void execCmd(char *request,t_db *db)
 {
-	char *tok;
-	tok = strtok(request,DELIM);
+		char *tok;
+		char *tableName ,*type;
+		t_table *newTable;
+		char *arg[30];
+		int elem = 0;
+		tok = strtok(request," ");
+		while (tok)
+		{				
+			arg[elem] = malloc(MAX_TABLE_NAME_LEN);
+			 if(!arg[elem])
+			 {
+			 	int i;
+			 	for( i = 0;i< elem; i++)
+			 		free(arg[i]);
+			 	deleteDB(db);
+			 	return;
+			 }
+			 strcpy(arg[elem++],tok);
+			 tok = strtok(NULL," ");	
+		}//end while
+		tableName = malloc(sizeof(MAX_TABLE_NAME_LEN));
+		if(!tableName)
+		{
+			deleteDB(db);
+			return;
+		}
+		strcpy(tableName,arg[1]);
+		
+		if(!type)
+		{
+			deleteDB(db);
+			free(tableName);
+			return;
+			// eliberare sir arg
+		}
+		strcpy(type,arg[2]);
+		newTable = createTable(db,tableName);
+		newTable->type = (t_cellType)type;
+		if(!newTable)
+		{
+			printf("nu s a alocat\n");
+			deleteDB(db);
+			return;
+		}
+		newTable->type = (t_cellType)type;
+		int index = 3;
+		for(; index < elem ;index++){
+			insertColumn(newTable,arg[index]);
+		}
 
-		char *tableName;
-		strcpy(tableName,"Students");
-		db->tables = createTable(db,tableName);	
-		insertColumn(db->tables,"First_Name");
-		insertColumn(db->tables,"Last_Name");
+		//if(newTable->type == STRING)
+			printDB(db,"STRING");
+		//if(newTable->type == INT)
+		//	printDB(db,INT);
+		
+		// pentru adaugare in tabel a valorilor pt fiecare coloana
+		if (!strcmp(arg[0],"ADD"))
+		{
+			int argINT[100];
+			char **argSTRING;	
+			char *tableName = malloc(sizeof(MAX_TABLE_NAME_LEN));
+			if(!tableName)
+			{
+				deleteDB(db);
+				return;
+			}
+			strcpy(tableName,arg[1]);
+			int index ;
+			//find table with tableName
+			t_table *searchTable = db->tables;
+			for(;searchTable != NULL;searchTable = searchTable->next)
+				if (!strcmp(searchTable->name,tableName))
+					break;
+
+			//if(searchTable->type == STRING)
+			
+			for(int i = 2; i < elem ;i++)
+				argSTRING[i-2] = arg[i];
+			     
+			
+			if(searchTable->type == INT)
+				for(int i = 2; i < elem ;i++)
+				
+			for(index = 2; index < elem ;index++)
+				/*if(searchTable->type == STRING)*/addString(db,tableName,argSTRING,elem-2);
+					
+				//else
+				//	addInt(db,tableName,argINT);
+			//if(newTable->type == STRING)
+				printDB(db,"STRING");
+			//if(newTable->type == INT)
+			//	printDB(db,INT);
+		
+		}
+		if (!strcmp(arg[0],"SEARCH"))
+		{
+			char *colName = malloc(MAX_COLUMN_NAME_LEN);
+			if(!colName)
+			{
+				free(tableName);
+				deleteDB(db);
+				return;
+			}
+			strcpy(colName,arg[2]);
+			// prelucare relatie dintre coloana si valoare data
+			//value arg[4];
+		}
+
+	for(int i = 0;i< elem; i++)
+		 free(arg[i]);
+
 }
 
 int main()
 {
 	t_db *db = initDB("SCHOOL");
 
-	char request[100];
-	strcpy(request,"CREATE Students First_Name LastName");
-	
+	char request[MAX_CMD_LEN];
+	int size = MAX_CMD_LEN;
+
+	char **relatii;
+	int i;
+	relatii = malloc(sizeof(char*)*6);
+	if(!relatii)
+	{
+		deleteDB(db);
+		return 1;
+	}
+	for(i = 0; i < 6; i++)
+	{
+		relatii[i] = malloc(3);
+		if(!relatii[i])
+		{
+			int j;
+			for(j = 0 ;j < i;j++)
+				free(relatii[j]);
+			free(relatii);
+			return 1;
+		}
+	}
+	strcpy(relatii[0],"<");
+	strcpy(relatii[1],"<=");
+	strcpy(relatii[2],">");
+	strcpy(relatii[3],">=");
+	strcpy(relatii[4],"!=");
+	strcpy(relatii[5],"==");
+	/*
+	fgets(request,sizeof(request),stdin);
 	execCmd(request,db);
-	int val[3];
-	val[0] = 1;
-	val[1] = 2;
-	addInt(db,"Students",val);
-
-	val[0] = 3;
-	val[1] = 4;
-	addInt(db,"Students",val);
-
-	printDB(db,"INT");
+	fgets(request,sizeof(request),stdin);
+	execCmd(request,db);
+	*/
+	t_table *t = createTable(db,"Stud");
+	insertColumn(t,"nume");
+	char **val;
+	val = malloc(2*sizeof(char*));
+	val[0] = malloc(30);
+	strcpy(val[0],"pla");
+	addString(db,"Stud",val,1);
+	printDB(db,"STRING");
 	deleteDB(db);
 
+	free(val[0]);
+	free(val);
+	free(relatii);
 	return 0;
 }
